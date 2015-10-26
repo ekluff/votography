a3b98be782846fb6be522488eff2972bd96b00ce
Evan Clough <clough.evan@gmail.com>
2015-10-26 12:12:32 -0700

    Set up database relationships.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,37 @@
 class AnswersController < ApplicationController
+  def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer, notice: 'Answer was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /answers/1
+  def update
+    if @answer.update(answer_params)
+      redirect_to @answer, notice: 'Answer was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /answers/1
+  def destroy
+    @answer.destroy
+    redirect_to answers_url, notice: 'Answer was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_answer
+      @answer = Answer.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def answer_params
+      params.require(:answer).permit(:text, :question_id, :votes)
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :answers, inverse_of: :question
+
+  validates :text, presence: true
 end
